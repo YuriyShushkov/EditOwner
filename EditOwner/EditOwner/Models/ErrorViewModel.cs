ef89c8e4118b926dd72e06df1a987fd081a54cca
Yuriy Shushkov <shushkov@gmail.com>
2017-10-11 18:45:33 +0300

    Init by template

---
+++ EditOwner/EditOwner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EditOwner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
